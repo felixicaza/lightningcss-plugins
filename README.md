89de9e329901450ea3459866f5518f0f3e093419
Felix Icaza <fx.joliett17@gmail.com>
2025-03-08 16:10:01 -0600

    chore: init project

---
+++ README.md
@@ -0,0 +1,23 @@
+# ⚡ LightningCSS Plugins
+
+This repository is dedicated to housing a collection of plugins for [LightningCSS](https://lightningcss.dev), a powerful and efficient CSS processor.
+
+## 📖 Introduction
+
+[LightningCSS](https://lightningcss.dev) is a modern CSS processor that offers a wide range of features for optimizing and transforming your CSS. This monorepo aims to provide a centralized location for various plugins that extend the functionality of LightningCSS.
+
+## 📦 Packages
+
+This repository currently includes the following packages:
+
+- **[pxtorem](packages/pxtorem)**: A plugin to convert pixel units to rem units in your CSS.
+
+Each package is located in the [`packages`](packages) directory and follows a consistent structure for ease of development and maintenance.
+
+## 🤝 Contributing
+
+If you wish to contribute to this project, you can do so by reading the [contribution guide](CONTRIBUTING.md).
+
+## 📄 License
+
+This project is licensed under the MIT License. See the [license file](LICENSE) for more details.
---
+++ eslint.config.ts
@@ -0,0 +1,48 @@
+// @ts-check
+
+import type { Linter } from 'eslint'
+
+import eslintPluginJs from '@eslint/js'
+import eslintPluginJsonc from 'eslint-plugin-jsonc'
+import eslintPluginPackageJson from 'eslint-plugin-package-json/configs/recommended'
+import eslintPluginYml from 'eslint-plugin-yml'
+import globals from 'globals'
+import neostandard, { plugins as eslintPlugins, resolveIgnoresFromGitignore } from 'neostandard'
+
+export default [
+  { ignores: resolveIgnoresFromGitignore() },
+  { languageOptions: { globals: { ...globals.browser, ...globals.node } } },
+  eslintPluginJs.configs.recommended,
+  ...neostandard({
+    noJsx: true,
+    noStyle: true,
+    ts: true
+  }),
+  eslintPlugins.promise.configs['flat/recommended'],
+  eslintPlugins['@stylistic'].configs['recommended-flat'],
+  eslintPlugins.n.configs['flat/recommended'],
+  ...eslintPluginJsonc.configs['flat/recommended-with-json'],
+  eslintPluginPackageJson,
+  ...eslintPluginYml.configs['flat/recommended'],
+  {
+    rules: {
+      '@stylistic/brace-style': ['error', '1tbs', { allowSingleLine: true }],
+      '@stylistic/no-multi-spaces': ['error', { ignoreEOLComments: false }],
+      '@stylistic/arrow-parens': ['error', 'always'],
+      '@stylistic/quote-props': ['error', 'as-needed'],
+      '@stylistic/comma-dangle': ['error', 'never'],
+      '@stylistic/operator-linebreak': [
+        'error',
+        'after',
+        { overrides: { '?': 'before', ':': 'before' } }
+      ],
+
+      // disabled due this rule not support workspaces
+      // reference: https://github.com/eslint-community/eslint-plugin-n/issues/209
+      'n/no-extraneous-import': ['off'],
+
+      'yml/indent': ['error', 3, { indicatorValueIndent: 2 }],
+      'yml/quotes': ['error', { prefer: 'double' }]
+    }
+  }
+] satisfies Linter.Config[]
