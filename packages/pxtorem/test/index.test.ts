3eca46c7c464ba8448c42b98d929a89156c4c8a8
Felix Icaza <fx.joliett17@gmail.com>
2025-04-01 21:43:04 -0600

    test(pxtorem): update snapshots and tests to use 'pixel' terminology and add new test cases for custom configurations

--- packages/pxtorem/test/index.test.ts
+++ packages/pxtorem/test/index.test.ts
@@ -3,7 +3,7 @@ import { composeVisitors, transform } from 'lightningcss'
 import pxtorem from '../dist/index'
 
 describe('pxtorem plugin', () => {
-  it('should convert px to rem', () => {
+  it('should convert pixel to rem', () => {
     const css = 'div { margin: 16px; }'
 
     const output = transform({
@@ -38,7 +38,7 @@ describe('pxtorem plugin', () => {
     expect(output).toMatchSnapshot()
   })
 
-  it('should convert px to rem in media queries', () => {
+  it('should convert pixel to rem in media queries', () => {
     const css = '@media (min-width: 768px) { div { margin: 16px; } }'
 
     const output = transform({
@@ -52,7 +52,7 @@ describe('pxtorem plugin', () => {
     expect(output).toMatchSnapshot()
   })
 
-  it('should convert px to rem in keyframes', () => {
+  it('should convert pixel to rem in keyframes', () => {
     const css = '@keyframes fadeIn { from { margin: 16px; } to { margin: 32px; } }'
 
     const output = transform({
@@ -66,7 +66,7 @@ describe('pxtorem plugin', () => {
     expect(output).toMatchSnapshot()
   })
 
-  it('should convert px to rem in custom properties', () => {
+  it('should convert pixel to rem in custom properties', () => {
     const css = ':root { --margin: 16px; } div { margin: var(--margin); }'
 
     const output = transform({
@@ -75,6 +75,76 @@ describe('pxtorem plugin', () => {
       minify: true,
       sourceMap: false,
       visitor: composeVisitors([pxtorem()])
+    }).code.toString()
+
+    expect(output).toMatchSnapshot()
+  })
+
+  it('should handle multiple pixel values', () => {
+    const css = 'div { margin: 16px 32px; }'
+
+    const output = transform({
+      filename: 'input.css',
+      code: Buffer.from(css),
+      minify: true,
+      sourceMap: false,
+      visitor: composeVisitors([pxtorem()])
+    }).code.toString()
+
+    expect(output).toMatchSnapshot()
+  })
+
+  it('should handle nested selectors', () => {
+    const css = 'div { & > span { margin: 16px; } }'
+
+    const output = transform({
+      filename: 'input.css',
+      code: Buffer.from(css),
+      minify: true,
+      sourceMap: false,
+      visitor: composeVisitors([pxtorem()])
+    }).code.toString()
+
+    expect(output).toMatchSnapshot()
+  })
+
+  it('should handle float pixel units', () => {
+    const css = 'div { margin: 43.3398589px; }'
+
+    const output = transform({
+      filename: 'input.css',
+      code: Buffer.from(css),
+      minify: true,
+      sourceMap: false,
+      visitor: composeVisitors([pxtorem()])
+    }).code.toString()
+
+    expect(output).toMatchSnapshot()
+  })
+
+  it('should handle custom configuration', () => {
+    const css = 'div { margin: 16px; }'
+
+    const output = transform({
+      filename: 'input.css',
+      code: Buffer.from(css),
+      minify: true,
+      sourceMap: false,
+      visitor: composeVisitors([pxtorem({ rootValue: 8, unitPrecision: 2 })])
+    }).code.toString()
+
+    expect(output).toMatchSnapshot()
+  })
+
+  it('should handle custom configuration with unitPrecision', () => {
+    const css = 'div { margin: 43.3398589px; }'
+
+    const output = transform({
+      filename: 'input.css',
+      code: Buffer.from(css),
+      minify: true,
+      sourceMap: false,
+      visitor: composeVisitors([pxtorem({ unitPrecision: 2 })])
     }).code.toString()
 
     expect(output).toMatchSnapshot()
