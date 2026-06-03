b63badf6003b81d141bacee906b7175fcde64b7e
Felix Icaza <fx.joliett17@gmail.com>
2026-06-03 09:50:01 -0600

    tests: add projects to vitest configuration and update tests location

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,6 +2,9 @@ import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
   test: {
+    projects: [
+      'packages/pxtorem'
+    ],
     coverage: {
       provider: 'v8',
       reporter: ['lcov'],
