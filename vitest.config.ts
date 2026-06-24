e0ed288349e219131acc754c0aba50b8a3d08941
Felix Icaza <fx.joliett17@gmail.com>
2026-06-24 09:31:05 -0600

    chore(tests): remove coverage configuration and related dependencies

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,12 +4,6 @@ export default defineConfig({
   test: {
     projects: [
       'packages/pxtorem'
-    ],
-    coverage: {
-      provider: 'v8',
-      reporter: ['lcov'],
-      include: ['**/src/**/*.ts'],
-      exclude: ['**/node_modules/**', '**/test/**', '**/dist/**']
-    }
+    ]
   }
 })
